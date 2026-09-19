f4d14e673d317d38f0a194c4c3ece9bac845e6a7
samayssa <arpittyagityagi70@gmail.com>
2026-09-19 17:42:51 +0530

    Add files via upload

--- miniapp_backend/db.py
+++ miniapp_backend/db.py
@@ -13,7 +13,7 @@ _pool: asyncpg.Pool | None = None
 async def connect() -> asyncpg.Pool:
     global _pool
     if _pool is None:
-        _pool = await asyncpg.create_pool(dsn=DATABASE_URL, min_size=1, max_size=10)
+        _pool = await asyncpg.create_pool(dsn=DATABASE_URL, min_size=0, max_size=3)
     return _pool
 
 
